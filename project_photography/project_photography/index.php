fef77fc4bb4446702130ae6761af382211080100
ANIKETBORSE-01 <borseaniket6@gmail.com>
2024-09-08 00:13:59 +0530

    Add files via upload

---
+++ project_photography/project_photography/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Aniket</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/04563a7b0e.js" crossorigin="anonymous"></script>
+
+</head>
+
+
+<body  style="background ;">
+
+<!-- Naav baar -->
+<section class="fixe" >
+<nav class="navbar navbar-expand-lg navbar-light "   style="background-color: black" >
+  <a class="navbar-brand font-weight-bold text-white menu2 " href="#">WedsCapture</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent"  >
+    <ul class="navbar-nav mr-auto " >
+      <li class="nav-item active" style="margin-left:300px;" >
+        <a class="nav-link text-white   menu1 h5 hove"  href="index.php">Home </a>
+      </li>
+    
+       
+      <li class="nav-item" >
+        <a class="nav-link  text-white    h5 hove " href="About.php">About Us</a>
+      </li>
+       <li class="nav-item" >
+        <a class="nav-link  text-white    h5 hove " href="service.php">Service</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link  text-white    h5 hove " href="contact.php">Contact</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link  text-white    h5 hove " href="feedback.php">Feedback</a>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
+</section>
+<!-- ************************************************************************************************************************************************************ -->
+
+
+<!-- carousel  -->
+
+<!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./images/mistu1.jpg" class="d-block img-fluid " style="height: 800px; width:100%;background-repeat: no-repeat;background-size: 100% 100%;">
+    </div>
+    <div class="carousel-item">
+      <img src="./images/mistu2.jpg" class="d-block img-fluid " style="height: 800px; width:100%;background-repeat: no-repeat;background-size: 100% 100%;">
+    </div>
+    <div class="carousel-item">
+      <img src="./images/mistu4.jpg" class="d-block img-fluid " style="height: 800px; width:100%;background-repeat: no-repeat;background-size: 100% 100%;">
+    </div>
+  </div>
+ <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>  -->
+
+
+<!-- backgroun image section -->
+<section>
+	<div class="back">
+		<h1 style=" position: relative; top:500px ; text-align: center;  color:white;  font-size: 60px; " >Your Movements,Our Responsibility...
+		</h1>
+		<p style=" position: relative; top:500px ;text-align: center; color: white;  font-size:20px">Taking an image,freezing movements reveals how to reach reality truly is.......!</p>
+	</div>
+</section>
+
+
+
+<!-- ********************************************************************************************************************************************* -->
+
+<div class="container">
+	<div class="row">
+		<div class="col-12 font-weight-bold text-center " style="color:#121154">
+			<h1 > Hello, I am Aniket Borse<br>
+					A Wedding & Lifestyle Photographer
+			</h1>
+			<h5 style=> Hello i am Aniket , a wedding photographer based in Rome, My passion is to document the<br>
+				Fleeting moments of love to create Timeless, beautiful images as memories, i love capturing<br>
+				weddings.
+
+			</h5>
+			
+		</div>
+	</div>
+</div>
+</div>
+
+<!-- *************************************************** v************************************************************************************************* -->
+
+<!-- Slider -->
+<style>
+	.carousel-item img {
+    transition: opacity 0.5s ease;
+    height: 540px;  
+    width: 100% !important;
+  }
+  .carousel-item img {
+    opacity: 0.7;
+}
+.carousel-caption {
+	text-align:center;
+  position: relative;
+  top:200px !important;
+    color: #000000 !important;
+    text-shadow: 0 1px 2px rgba(255, 255, 255, 0.3);
+  }
+</style>
+<section style="margin-top:-100px" >
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel" >
+      <div class="carousel-inner">
+        <div class="carousel-caption  ">
+          <h1>Welcome To <br> The World of Photography</h1>
+          <p class="w-100">Bringing Your Visions To Life With Precision & passion</p>
+        </div>
+        <div class="carousel-item active">
+          <img src="./images/photo1.jpg" data-aos=" zoom-in" class="d-block w-100 " alt="..." style="height:700px">
+         
+        </div>
+        <div class="carousel-item">
+          <img src="images/.jpg" data-aos=" zoom-in" class="d-block w-100" alt="...">
+
+        </div>
+        
+      </div>
+      <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span> 
+      </button>-->
+      <!-- <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button> -->
+    </div>
+  </section>
+
+<Section>
+</div>
+
+		<p class="text-white font-weight-bold text-center mt-4 ">Since 2008, I’ve been providing my clients with quality photography and retouching services. Some of my works were awarded on a national level. Below are some facts about me.</p>
+
+	<div class="container mt-5 ">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="card shadow" style="height:200px" >
+						<div class="card-body text-center">
+				  	 <div class="logo_bg text-center h1">  </div><br><br><br>
+						<h1 class="font-weight-bold text-center" style="margin-top:-60px">5</h1>
+				    <h5 class="card-title   font-weight-bold"  > Photography Awards</h5>
+				   
+				   
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card shadow" style="height:200px">
+						<div class="card-body text-center">
+				  	 <div class="logo_bg text-center"> </div><br><br><br>
+						<h1 class="font-weight-bold text-center"  style="margin-top:-40px">115</h1>
+				    <h5 class="card-title  mt-2 font-weight-bold"  > Photo Shoots</h5>
+				   
+				   
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card shadow" style="height:200px">
+						<div class="card-body text-center">
+
+				  	 <div class="logo_bg text-center font-weight-bold"  >  </div><br><br><br>
+						<h1 class="font-weight-bold text-center"  style="margin-top:-40px">3</h1>
+				    <h5 class="card-title font-weight-bold  mt-2">Years of Experience</h5>
+				   
+				   
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="card shadow" style="height:200px">
+						<div class="card-body text-center">
+
+				  	 <div class="logo_bg text-center  font-weight-bold " > </div><br><br><br>
+				  	 	<h1 class="font-weight-bold text-center"  style="margin-top:-40px">50</h1>
+				    <h5 class="card-title  font-weight-bold mt-2">Annual Clients </h5>
+									   
+				   
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</Section>
+<!-- *********************************************************************************************************************************************************** -->
+
+    <!-- Footer Start -->
+   <div style="height: 320px; width:100%; background-color: black;">
+    	<div class="container-fluid">
+    		<div class="row">
+    			<div class="col-md-4">
+    				<h4 class="font-weight-bold text-white mt-5">WedsCapture:<br>Wedding Photography Professionals	</h4>
+    				<p class="text-white font-weight-bold text-centers mt-4">Plan You Wedding with WedsCapture an Indian Wedding planning Website</p>
+    				<!-- <button class="btn btn-outline-warning " style="margin-left:80px;">Register</button> -->
+    			</div>
+    			<div class="col-md-4">
+    				<h1 class="font-weight-bold  text-white mt-4">Our locations:</h1>
+    				<ul type="circle " class="text-white">
+    					<li>Akurdi</li>
+    					<li>Pune</li>
+    					<li>Ravet</li>
+    					
+    				</ul>
+    			</div>
+    			<div class="col-md-4">
+    				<!-- <h2 class="text-white  mt-4 font-weight-bold">Our Newsletter</h2>
+    				<input type="text" name="" placeholder=" enter your name ">
+    				<button class="btn btn-outline-warning">Subscribe</button> -->
+<br>
+    				  <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Contact</h4>
+                    <p class="mb-2 text-white"><i class="fa fa-map-marker-alt me-3 text-white"></i>Ahmednagar,414001</p>
+                 
+                    <p class="mb-2 text-white"><i class="fa fa-envelope me-3 text-white"></i>Aniket@gmail.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                   
+                    </div>
+                </div>
+    			</div>
+    		</div>
+    	</div>
+    </div> 
+
+
+    
+	<script>
+    AOS.init();
+  </script>
+	
+</body>
+</html>
+
